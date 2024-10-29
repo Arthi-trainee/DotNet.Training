f20e3c3e58afc8cc4248bd46e21810eee585d1cc
Arthi Muddisetty <arthim@infinite.com>
2024-10-29 22:10:44 +0530

    Assignment Submitted

---
+++ Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers to perform arithematic opearations");
+            int num1 = Console.Read();
+            Console.ReadLine();
+            int num2 = Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("Enter opearator to perform arithematic opearations");
+            Console.ReadLine();
+            char opearator = Console.Read();
+            switch (opearator) {
+                case '+':
+
+                        }
+
+
+
+
+        }
+    }
+}
