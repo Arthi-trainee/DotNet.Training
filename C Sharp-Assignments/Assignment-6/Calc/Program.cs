75704ecfdb20b2f74f2051337d67247ab72f9469
Arthi Muddisetty <arthim@infinite.com>
2024-11-17 11:11:35 +0530

    Assignment 6 submitted successfully

---
+++ C Sharp-Assignments/Assignment-6/Calc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using CalculatorLibrary;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter user name:");
+
+            string user_name = Console.ReadLine();
+
+            Console.WriteLine("Enter Age of the user:");
+
+            int user_age = Convert.ToInt32(Console.ReadLine());
+
+            Class1 calc_lib_obj= new Class1();
+
+            calc_lib_obj.CalculateConcession(user_name, user_age);
+
+            Console.Read();
+
+        }
+    }
+}
